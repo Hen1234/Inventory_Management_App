e4c645b2644565da4e9fb615323c96b234661d60
Hen Mor <henmor93@gmail.com>
2020-10-07 10:54:27 +0300

    Build the Front-End resources to the public folder

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-   //TODO: change to index.html of Angular
-  res.render('index', { title: 'Express' });
+  res.render('../public/index.html');
 });
 
 module.exports = router;
